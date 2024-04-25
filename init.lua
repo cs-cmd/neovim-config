08007aa20de37984ce8ae2058746330d0ca23586
Ray O. (VICTUS) <rayoneill.97@gmail.com>
2024-04-25 13:29:53 -0400

    Removed `stylua` LSP from listing.

--- init.lua
+++ init.lua
@@ -363,9 +363,6 @@ require("lazy").setup({
 			require("mason").setup()
 
 			local ensure_installed = vim.tbl_keys(servers or {})
-			vim.list_extend(ensure_installed, {
-				"stylua",
-			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
 			require("mason-lspconfig").setup({
